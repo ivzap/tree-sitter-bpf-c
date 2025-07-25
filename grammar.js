34b68e99c5108b1968d0690d55fc6fcfe21844e0
Ivan James Zaplatar <ivzap@fb.com>
2025-07-24 17:12:24 -0700

    implemented grammer required to lint bpf maps

--- grammar.js
+++ grammar.js
@@ -13,6 +13,7 @@ module.exports = grammar(C, {
   name: "bpf_c",
 
   rules: {
+
     sec_specifier: $ => seq(
       'SEC',
       '(',
@@ -28,5 +29,42 @@ module.exports = grammar(C, {
       field('declarator', $._declarator),
       field('body', $.compound_statement),
     ),
+    
+    _field_declaration_list_item: $ => choice(
+      $.uint_bpf_macro,
+      $.field_declaration,
+      $.preproc_def,
+      $.preproc_function_def,
+      $.preproc_call,
+      alias($.preproc_if_in_field_declaration_list, $.preproc_if),
+      alias($.preproc_ifdef_in_field_declaration_list, $.preproc_ifdef),
+    ),
+
+    uint_bpf_macro: $ => choice(
+      seq(
+        '__uint',
+        '(',
+        $.identifier,
+        ',',
+        $.sizeof_expression,
+        ')',
+        ';'
+      ),
+      seq(
+        '__uint',
+        '(',
+        optional(commaSep($.identifier)),
+        ')'
+        )
+    ),
+
   }
 });
+
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
+
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(',', rule)));
+}
